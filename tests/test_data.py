3a6b37bd2c9620a808b128e16fd1ca9de92a09c4
HarrisNH <s224397@dtu.dk>
2025-01-20 14:48:28 +0100

    fix github actions path

--- tests/test_data.py
+++ tests/test_data.py
@@ -10,6 +10,7 @@ from tests.__init__ import _PATH_DATA  # Adjust this import to your actual utils
 def test_load_and_preprocess_data():
     """Test that the data is loaded and preprocessed correctly."""
     # Load and preprocess the data
+    print(f"{_PATH_DATA}/raw/sp500_companies.csv")
     column_transformer, X_train, X_val, X_test, y_train, y_val, y_test, sector_names = (
         load_and_preprocess_data(f"{_PATH_DATA}/raw/sp500_companies.csv")
     )
