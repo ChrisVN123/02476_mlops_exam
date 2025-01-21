7c5dc70a803fee3076c6dfac95cab19c8b709a8f
FabianSWuertz <s224411@student.dtu.dk>
2025-01-21 12:11:35 +0100

    apitest3

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 
 def test_valid_initials():
-    response = client.get("/predict/APPL")
+    response = client.get("/predict/AAPL")
     assert response.status_code == 200
     assert "prediction" in response.json()
     assert "correct" in response.json()
@@ -21,6 +21,6 @@ def test_invalid_initials():
 
 
 def test_error():
-    response = client.get("/predict/APPL?model_path=nonexistent_model.onnx")
+    response = client.get("/predict/AAPL?model_path=nonexistent_model.onnx")
     assert response.status_code == 500
     assert response.json() == {"detail": "An unexpected error occurred."}
