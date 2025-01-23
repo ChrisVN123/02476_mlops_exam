b39f891a31077911d8489f5622dfc5da20037fd0
ChrisVN123 <christianvanell@icloud.com>
2025-01-23 12:02:56 +0100

    workflow

--- tests/performancetests/test_model.py
+++ tests/performancetests/test_model.py
@@ -21,7 +21,7 @@ def load_model():
     )
 
     # Fetch and download the artifact
-    artifact = api.artifact("dtu_mlops_24/wandb-registry-ExamMLOps/test_collection:v3")
+    artifact = api.artifact("test_collection:v3")
     artifact.download(root=logdir)
 
     # Load the model checkpoint
